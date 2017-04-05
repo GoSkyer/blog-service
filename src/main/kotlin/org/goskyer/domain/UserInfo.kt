10762abdd4c0bc6f8da8c50641b6e4549c201973
guozhong@9188.com <guozhong@9188.com>
2017-04-05 20:29:00 +0800

    创建userInfo pojo

---
+++ src/main/kotlin/org/goskyer/domain/UserInfo.kt
@@ -0,0 +1,17 @@
+package org.goskyer.domain
+
+/**
+ * Created by zohar on 2017/4/5.
+ * desc:
+ */
+data class UserInfo(
+        var userInfoId:Int,
+        var userName:String,
+        var userAvatar:String,
+        var userGithub:String,
+        var userWechat:String,
+        var userQQ:String,
+        var userMd:String,
+        var userResume:String,
+        var userId:Int
+)
